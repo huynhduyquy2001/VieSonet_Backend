818d0b817470a31319aa1a30a8a846e99a49f120
Huynh-Duy-Quy <huynhduyquy2001@gmail.com>
2023-05-30 23:58:46 +0700

    23:58_30-05-2023

---
+++ VieSonet/src/main/java/com/viesonet/VieSonetApplication.java
@@ -0,0 +1,13 @@
+package com.viesonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VieSonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VieSonetApplication.class, args);
+	}
+
+}
