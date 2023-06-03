a437605a3bb5dca319b004f21100108b97a4e536
Vyx-stack <133791259+Vyx-stack@users.noreply.github.com>
2023-06-03 09:13:35 +0700

    DAO v1
    ( 3-6-2023 | 9:09 )

---
+++ VieSonet/src/main/java/com/viesonet/dao/DanhSachKetBanDAO.java
@@ -0,0 +1,9 @@
+package com.viesonet.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.viesonet.entity.DanhSachKetBan;
+
+public interface DanhSachKetBanDAO extends JpaRepository<DanhSachKetBan, String> {
+
+}
