4eefac7e8d5bc4c962384356a0bd7db482c7c66f
Lý Tường <lyquoctuong272@gmail.com>
2023-06-05 17:15:12 +0700

    update stored_procedure 5:14_05/06/2023

--- file_SQL/Stored_procedure.sql
+++ file_SQL/Stored_procedure.sql
@@ -1,63 +1,112 @@
-﻿--1.Thống kê số bài viết vi phạm theo tháng--
---Lệnh tạo
-CREATE PROC sp_ThongKeBaiVietViPham(@month int)
-as begin
-select 
-       COUNT(DISTINCT maBaiViet) SoBaiVietViPham
-from BaiVietViPham
-where MONTH(ngayToCao) = @month --> Điều kiện
-end
---Lệnh chạy chương trình xem kết quả
-EXEC sp_ThongKeBaiVietViPham 05 -->Truyền tham số để xét điều kiện
---Lệnh xóa proc
-DROP PROC sp_ThongKeBaiVietViPham
+﻿CREATE PROCEDURE sp_ThongKeBaiVietViPhamTheoThang (
+    @Nam INT
+)
+AS
+BEGIN
 
---1.Thống kê số lượt báo cáo theo tháng--
---Lệnh tạo
-CREATE PROC sp_ThongKeSoLuotBaoCao(@month int)
-as begin
-select 
-       COUNT(DISTINCT maToCao) SoLuotBaoCao
-from BaiVietViPham
-where MONTH(ngayToCao) = @month --> Điều kiện
-end
---Lệnh chạy chương trình xem kết quả
-EXEC sp_ThongKeSoLuotBaoCao 05 -->Truyền tham số để xét điều kiện
---Lệnh xóa proc
-DROP PROC sp_ThongKeSoLuotBaoCao
+    -- Tạo bảng tạm chứa 12 tháng
+    WITH Thang(Thang) AS (
+        SELECT 1
+        UNION ALL
+        SELECT Thang + 1 FROM Thang WHERE Thang < 12
+    ), 
+    -- Lấy thông tin số lượng bài viết vi phạm theo từng tháng trong năm
+    ThongKe(Thang, SoBaiVietViPham) AS (
+        SELECT MONTH(ngayToCao), COUNT(DISTINCT maBaiViet)
+        FROM BaiVietViPham
+        WHERE YEAR(ngayToCao) = @Nam
+        GROUP BY MONTH(ngayToCao)
+    )
+    -- Kết hợp thông tin với bảng tạm chứa 12 tháng
+    SELECT Thang.Thang, ISNULL(SoBaiVietViPham, 0) AS SoBaiVietViPham
+    FROM Thang
+    LEFT JOIN ThongKe ON Thang.Thang = ThongKe.Thang
+
+END
+EXEC sp_ThongKeBaiVietViPhamTheoThang 2023
+
+
+CREATE PROCEDURE sp_ThongKeSoLuotBaoCaoTheoThang (
+    @Nam INT
+)
+AS
+BEGIN
+
+    -- Tạo bảng tạm chứa 12 tháng
+    WITH Thang(Thang) AS (
+        SELECT 1
+        UNION ALL
+        SELECT Thang + 1 FROM Thang WHERE Thang < 12
+    ), 
+    -- Lấy thông tin số lượng bài viết vi phạm theo từng tháng trong năm
+    ThongKe(Thang, SoLuotBaoCao) AS (
+        SELECT MONTH(ngayToCao), COUNT(DISTINCT maToCao)
+        FROM BaiVietViPham
+        WHERE YEAR(ngayToCao) = @Nam
+        GROUP BY MONTH(ngayToCao)
+    )
+    -- Kết hợp thông tin với bảng tạm chứa 12 tháng
+    SELECT Thang.Thang, ISNULL(SoLuotBaoCao, 0) AS SoLuotBaoCao
+    FROM Thang
+    LEFT JOIN ThongKe ON Thang.Thang = ThongKe.Thang
+
+END
+EXEC sp_ThongKeSoLuotBaoCaoTheoThang 2023
 
 --2.Thống kê số lượng người đăng ký theo tháng--
---Lệnh tạo
-CREATE PROC sp_ThongKeSoLuongNguoiDangKy(@month int)
-as begin
-select 
-       COUNT(DISTINCT sdt) SoLuongDangKy
-from nguoiDung
-where MONTH(ngayTao) = @month --> Điều kiện
-end
---Lệnh chạy chương trình xem kết quả
-EXEC sp_ThongKeSoLuongNguoiDangKy 01 -->Truyền tham số để xét điều kiện
---Lệnh xóa proc
-DROP PROC sp_ThongKeSoLuongNguoiDangKy
+CREATE PROCEDURE sp_ThongKeSoLuongDangKyTheoThang (
+    @Nam INT
+)
+AS
+BEGIN
 
+    -- Tạo bảng tạm chứa 12 tháng
+    WITH Thang(Thang) AS (
+        SELECT 1
+        UNION ALL
+        SELECT Thang + 1 FROM Thang WHERE Thang < 12
+    ), 
+    -- Lấy thông tin số lượng bài viết vi phạm theo từng tháng trong năm
+    ThongKe(Thang, SoLuongDangKy) AS (
+        SELECT MONTH(ngayTao), COUNT(DISTINCT sdt)
+        FROM nguoiDung
+        WHERE YEAR(ngayTao) = @Nam
+        GROUP BY MONTH(ngayTao)
+    )
+    -- Kết hợp thông tin với bảng tạm chứa 12 tháng
+    SELECT Thang.Thang, ISNULL(SoLuongDangKy, 0) AS SoLuongDangKy
+    FROM Thang
+    LEFT JOIN ThongKe ON Thang.Thang = ThongKe.Thang
+
+END
+--Lệnh chạy chương trình xem kết quả
+EXEC sp_ThongKeSoLuongDangKyTheoThang  @nam = 2023 -->Truyền tham số để xét điều kiện
 
 
 --3.Thống kê bài viết có số lượt thích cao nhất theo ngày tháng năm-- 
 --Lệnh tạo
-CREATE PROC sp_ThongKeSoLuotThichCaoNhat(@month date)
-as begin
-select top 10 BaiViet.hinhAnh HinhAnh,
-       BaiViet.maBaiViet MaBaiViet,
-	   nguoiDung.hoTen NguoiDang,
-	   BaiViet.moTa MoTa,
-	   COUNT(DanhSachYeuThich.maBaiViet) LuotThich
-from DanhSachYeuThich, BaiViet, nguoiDung
-where DanhSachYeuThich.maBaiViet=BaiViet.maBaiViet and BaiViet.sdt = nguoiDung.sdt
-and DanhSachYeuThich.ngayYeuThich = @month --> Điều kiện
-group by BaiViet.hinhAnh, BaiViet.maBaiViet, nguoiDung.hoTen, BaiViet.moTa
-order by LuotThich DESC
-end
---Lệnh chạy chương trình xem kết quả
-EXEC sp_ThongKeSoLuotThichCaoNhat '2023-05-22' -->Truyền tham số để xét điều kiện
---Lệnh xóa proc
-DROP PROC sp_ThongKeSoLuotThichCaoNhat 
+CREATE PROCEDURE sp_ThongKeSoLuotThichCaoNhat
+    @month DATE
+AS
+BEGIN
+    SELECT TOP 10 
+        BaiViet.hinhAnh AS HinhAnh,
+        BaiViet.maBaiViet AS MaBaiViet,
+        nguoiDung.hoTen AS NguoiDang,
+        BaiViet.moTa AS MoTa,
+        COUNT(DanhSachYeuThich.maBaiViet) AS LuotThich
+    FROM 
+        DanhSachYeuThich
+        JOIN BaiViet ON DanhSachYeuThich.maBaiViet = BaiViet.maBaiViet
+        JOIN nguoiDung ON BaiViet.sdt = nguoiDung.sdt
+    WHERE 
+        DanhSachYeuThich.ngayYeuThich BETWEEN DATEFROMPARTS(YEAR(@month), MONTH(@month), 1) AND EOMONTH(@month)
+    GROUP BY 
+        BaiViet.hinhAnh,
+        BaiViet.maBaiViet,
+        nguoiDung.hoTen,
+        BaiViet.moTa
+    ORDER BY 
+        LuotThich DESC;
+END
+EXEC sp_ThongKeSoLuotThichCaoNhat '2023-05-22'
