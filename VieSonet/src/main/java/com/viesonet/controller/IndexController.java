2aab520f880b21cf09f896931c1d3dc6bf6d1775
Vyx-stack <133791259+Vyx-stack@users.noreply.github.com>
2023-06-12 23:28:29 +0700

    .

--- VieSonet/src/main/java/com/viesonet/controller/IndexController.java
+++ VieSonet/src/main/java/com/viesonet/controller/IndexController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.viesonet.dao.BaiVietDao;
+import com.viesonet.dao.BaiVietDAO;
 import com.viesonet.dao.NguoiDungDAO;
 import com.viesonet.dao.BanBeDAO;
 import com.viesonet.dao.CheDoDAO;
@@ -44,7 +44,7 @@ import com.viesonet.service.SessionService;
 @Controller
 public class IndexController {
 	@Autowired
-	BaiVietDao baiVietDao;
+	BaiVietDAO baiVietDao;
 
 	@Autowired
 	NguoiDungDAO nguoiDungDAO;
